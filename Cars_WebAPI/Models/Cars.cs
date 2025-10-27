f9b43f029d529688f4692b6b689138c83b799339
vshanovskiy <viktor6998@gmail.com>
2025-10-27 17:53:01 +0300

    create model cars

---
+++ Cars_WebAPI/Models/Cars.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cars_WebAPI.Models
+{
+    [Table("MyCars")]
+    public class Cars
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(5)]
+        public string Brand { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Model { get; set; }
+
+        [Phone]
+        public string Speed { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+        public int Data { get; set; }
+        public int Weight { get; set; }
+        public int OwnerId { get; set; }
+
+    }
+}
